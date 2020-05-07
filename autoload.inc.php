5655a31b7699076adbe9d4d1018ecb5df542762b
Thibault Lanier <me@thibault-lanier.fr>
2020-05-07 14:35:29 +0000

    Ajout du PDO de connexion à la BDD

--- autoload.inc.php
+++ autoload.inc.php
@@ -2,7 +2,7 @@
 
 function __autoload($class_name){
     if (strtolower($class_name) == "mypdo") {
-        require_once 'class/'.strtolower($class_name) . '.include.php';
+        require_once strtolower($class_name) . '.inc.php';
     } else {
         require_once 'class/'.strtolower($class_name) . '.class.php';
     }
